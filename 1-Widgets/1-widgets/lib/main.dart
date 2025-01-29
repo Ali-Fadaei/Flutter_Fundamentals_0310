044f98fd5e4c12ad12d2935004a527b6824b9ecc
Ali-Fadaei <Ali.Fadaei.424@gmail.com>
2025-01-29 17:03:23 +0330

    column layout added

--- 1-Widgets/1-widgets/lib/main.dart
+++ 1-Widgets/1-widgets/lib/main.dart
@@ -1,14 +1,25 @@
-import 'package:widgets/first_widget.dart';
+import 'package:widgets/column_layout.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey.shade400,
-        body: FirstWidget(),
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          color: Colors.brown,
+          child: ColumnLayout(),
+        ),
       ),
-    ),
-  );
-  // runApp(const MyApp());
+    );
+  }
 }
