98b6ec8a7b67580cdefde93aa0f3b650cab589c1
Ali-Fadaei <Ali.Fadaei.424@gmail.com>
2025-02-03 23:24:44 +0330

    Counter widget reverted to Stateless

--- 1-Widgets/2-widget_types/lib/counter.dart
+++ 1-Widgets/2-widget_types/lib/counter.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Counter extends StatefulWidget {
+class Counter extends StatelessWidget {
   //
   final int count;
 
@@ -9,38 +9,13 @@ class Counter extends StatefulWidget {
     required this.count,
   });
 
-  @override
-  State<Counter> createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
   //
-
-  @override
-  void initState() {
-    print('Counter init state fired!');
-    super.initState();
-  }
-
-  @override
-  void didUpdateWidget(covariant Counter oldWidget) {
-    print('Counter didUpdateWidget fired!');
-
-    print('oldWidget.count');
-    print(oldWidget.count);
-
-    print('widget.count');
-    print(widget.count);
-
-    super.didUpdateWidget(oldWidget);
-  }
-
   @override
   Widget build(BuildContext context) {
     print('Counter Build Method Fired!');
     return Center(
       child: Text(
-        'Current Count is: ${widget.count}',
+        'Current Count is: $count',
         style: TextStyle(
           fontSize: 24,
           fontWeight: FontWeight.bold,
