d4d8025aff9836ea0b48d3d4dce1eb625bcf4afa
Ali-Fadaei <Ali.Fadaei.424@gmail.com>
2025-02-15 18:49:57 +0330

    shop app part 4

--- 1-Widgets/4- shop_m3/lib/pages/store/store_page.dart
+++ 1-Widgets/4- shop_m3/lib/pages/store/store_page.dart
@@ -8,95 +8,118 @@ class StorePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: TextField(
-              controller: TextEditingController(),
-              decoration: InputDecoration(
-                prefixIcon: Icon(
-                  Icons.search,
-                ),
-                hintText: 'Search Here',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                ),
+    return ListView(
+      children: [
+        const SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: TextField(
+            controller: TextEditingController(),
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.search,
+              ),
+              hintText: 'Search Here',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100),
               ),
             ),
           ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Row(
-              children: [
-                Text(
-                  'Exclusive Offers',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            children: [
+              Text(
+                'Exclusive Offers',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
-                const Spacer(),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          SizedBox(
-            height: 320,
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                spacing: 8,
-                children: products.map((e) => ProductCard(data: e)).toList(),
               ),
-            ),
+              const Spacer(),
+            ],
           ),
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        SizedBox(
+          height: 320,
+          child: ListView.separated(
+            itemCount: products.length,
             padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Row(
-              children: [
-                Text(
-                  'Exclusive Offers',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
+            scrollDirection: Axis.horizontal,
+            separatorBuilder: (_, index) {
+              print('separetor builder executed. index: $index');
+              return const SizedBox(
+                width: 16,
+              );
+            },
+            itemBuilder: (_, index) {
+              print('item builder executed. index: $index');
+              return ProductCard(
+                data: products[index],
+              );
+            },
+          ),
+          // child: SingleChildScrollView(
+          //   scrollDirection: Axis.horizontal,
+          //   child: Row(
+          //     spacing: 8,
+          //     children: products.map((e) => ProductCard(data: e)).toList(),
+          //   ),
+          // ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            children: [
+              Text(
+                'Exclusive Offers',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
-                const Spacer(),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          SizedBox(
-            height: 320,
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                spacing: 8,
-                children:
-                    products.reversed.map((e) => ProductCard(data: e)).toList(),
               ),
-            ),
+              const Spacer(),
+            ],
           ),
-          const SizedBox(
-            height: 20,
-          ),
-        ],
-      ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        // SizedBox(
+        //   height: 320,
+        //   child: SingleChildScrollView(
+        //     scrollDirection: Axis.horizontal,
+        //     child: Row(
+        //       spacing: 8,
+        //       children:
+        //           products.reversed.map((e) => ProductCard(data: e)).toList(),
+        //     ),
+        //   ),
+        // ),
+        const SizedBox(
+          height: 20,
+        ),
+      ],
     );
   }
+  //   return SingleChildScrollView(
+  //     child: Column(
+  //       children: [
+  //
+  //       ],
+  //     ),
+  //   );
+  // }
 }
