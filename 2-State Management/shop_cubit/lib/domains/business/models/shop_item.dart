1f1bccd64e10bc327af29992952050fbb42a3cd7
Ali-Fadaei <Ali.Fadaei.424@gmail.com>
2025-05-02 13:45:42 +0330

    bloc architecture

---
+++ 2-State Management/shop_cubit/lib/domains/business/models/shop_item.dart
@@ -0,0 +1,10 @@
+import '/domains/business/models/product.dart';
+
+class ShopItem {
+  //
+  final Product product;
+
+  int count;
+
+  ShopItem({required this.product}) : count = 1;
+}
---
+++ 2-State Management/shop_cubit/lib/modules/category/category_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../../domains/business/models/category.dart';
+import 'category_card.dart';
+
+class CategoryPage extends StatelessWidget {
+  //
+  final List<CategoryData> categories;
+
+  const CategoryPage({super.key, required this.categories});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return GridView.count(
+      padding: const EdgeInsets.all(16),
+      crossAxisCount: (size.width / 200).floor(),
+      mainAxisSpacing: 4,
+      crossAxisSpacing: 4,
+      childAspectRatio: 200 / 320,
+      children: categories.map((e) => CategoryCard(data: e)).toList(),
+    );
+  }
+}
